cb0bbe0a4c10a0990b0568751ed86b6cf27fa50d
jagan0227 <j2a7g0a2n2003@gmail.com>
2023-09-10 20:45:18 +0530

    Update code and images

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ $(document).ready(function () {
     $("html").css("scrollBehavior", "smooth");
   });
   //like btn
-  $(".like-btn").click(function (){
-    document.getElementById("like-btn").innerHTML = "<i style='font-size:24px' class='fa'>&#xf004;</i> Liked";
+  $(".like-btn").click(function () {
+    document.getElementById("like-btn").innerHTML =
+      "<i style='font-size:24px' class='fa'>&#xf004;</i> Liked";
   });
 
   // toggle menu/navbar script
@@ -60,8 +61,6 @@ $(document).ready(function () {
     loop: true,
   });
 
-
-
   // owl carousel script
   $(".carousel").owlCarousel({
     margin: 20,
@@ -87,7 +86,118 @@ $(document).ready(function () {
 });
 
 // mail-form-actions
-function submitMail(){
+function submitMail() {
   alert("Thanks for your message...\nThe page will be reloaded.");
 }
 
+// project list
+document.addEventListener("DOMContentLoaded", function () {
+  const projects = [
+    {
+      name: "NVeg App",
+      link: "https://github.com/jagan0227/NVeg_App",
+      iconClass: "fa fa-egg",
+      description:
+        "(ONGOING) An app that used to buy fresh non-veg products like fish, chicken, mutton, and more.",
+    },
+    {
+      name: "Blind Master App",
+      link: "https://github.com/jagan0227/Blind_Master_App",
+      iconClass: "fa fa-blind",
+      description:
+        "An app that helps blind people hear what's in front of them.",
+    },
+    {
+      name: "Track My Wheels",
+      link: "https://github.com/jagan0227/Track_My_Wheels",
+      iconClass: "fa fa-bus",
+      description: "Bus Tracking app which will work on GPS and Google Map.",
+    },
+    {
+      name: "Book Hub App",
+      link: "https://github.com/jagan0227/Book-Hub-Android-App",
+      iconClass: "fa fa-book",
+      description:
+        "An Android application that shows the price of a book along with a description of the book.",
+    },
+    {
+      name: "Home Inventory Manager",
+      link: "https://github.com/jagan0227/Home-Inventory-Manager",
+      iconClass: "fas fa-home",
+      description:
+        "An application helps you keep track of your home's valuables.",
+    },
+    {
+      name: "Public Chat System",
+      link: "https://github.com/jagan0227/Public-Chat-System",
+      iconClass: "fas fa-sms",
+      description:
+        "This is a website where you can chat anonymously with the public.",
+    },
+    {
+      name: "Social Networking",
+      link: "https://github.com/jagan0227/Social-Networking",
+      iconClass: "fas fa-quote-left",
+      description: "A web application that connects people around the world.",
+    },
+    {
+      name: "ATM Simulation System",
+      link: "https://github.com/jagan0227/ATM-Simulation-System",
+      iconClass: "fa fa-credit-card",
+      description: "An application that simulates the work of an ATM.",
+    },
+  ];
+  const projectContainer = document.getElementById("projectContainer");
+
+  projects.forEach((project) => {
+    const card = document.createElement("a");
+    card.classList.add("card");
+    card.href = project.link;
+    card.target = "_blank";
+
+    card.innerHTML = `
+    <div class="box">
+    <i class="${project.iconClass}"></i>
+    <div class="text"><a href="${project.link}" target="_blank">${project.name}</a></div>
+    <p>${project.description}</p>
+</div>
+`;
+
+    projectContainer.appendChild(card);
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const skills = [
+    "Java",
+    "Kotlin",
+    "Python",
+    "C/C++",
+    "HTML/CSS",
+    "JavaScript",
+    "PHP",
+    "React Js",
+    "Node Js",
+    "MySQL",
+    "MongoDB",
+    "Firebase",
+    "Jetpack Compose",
+
+    // Add more skills as needed
+  ];
+
+  // Function to create skill cards
+  function createSkillCards() {
+    const skillContent = document.getElementById("skillContent");
+
+    skills.forEach((skill) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      card.textContent = skill;
+
+      skillContent.appendChild(card);
+    });
+  }
+
+  // Call the function to create skill cards
+  createSkillCards();
+});
